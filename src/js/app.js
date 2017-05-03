c55fec0c076181b3c0c74adcc1ad5ea7343321e4
Max Kirigetov <maximkirigetov@gmail.com>
2017-05-03 15:52:11 +0300

    first-commit

---
+++ src/js/app.js
@@ -0,0 +1,4 @@
+//=include lib/jquery.js
+//=include lib/slick.min.js
+//=include lib/select2.min.js
+//=include common.js
---
+++ src/js/common.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+	$('.js-select').select2({
+		placeholder: "Выберите город",
+		allowClear: true
+	});
+
+});
